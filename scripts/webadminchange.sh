43a2e0602ac084e982532a3454799713ec03dc60
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-14 07:54:44 -0400

    Update webadminchange.sh

--- scripts/webadminchange.sh
+++ scripts/webadminchange.sh
@@ -3,23 +3,23 @@ NC='\033[0m'
 
 while :
 do
-	read -r -p "Введите логин: " USERNAME
+	read -r -p "Digite seu login:" USERNAME
 	if [[ "$USERNAME" != "" ]]
 	then
 		while :
 		do
-			read -r -s -p "Введите пароль: " PASSWORD
+			read -r -s -p "Digite a senha: " PASSWORD
 			if [[ "$PASSWORD" != "" ]]
 			then
 				sed -i "s/'username' => '.*$/'username' => '$USERNAME',/g; s/'password' => '.*$/'password' => '$PASSWORD',/g"  app/models/User.php
 				break 2
 			else
-				echo; echo -e "${RED}Пароль не может быть пустым${NC}"
+				echo; echo -e "${RED}A senha não pode ficar vazia${NC}"
 				continue
 			fi
 		done
 	else
-		echo -e "${RED}Логин не может быть пустым${NC}"
+		echo -e "${RED}O login não pode ficar vazio${NC}"
 		continue
 	fi
 done

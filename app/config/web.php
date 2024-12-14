0914013cb7c522f06536f4ef85a510914939ae54
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-14 08:16:20 -0400

    Update web.php

--- app/config/web.php
+++ app/config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'Real Mikrotik Backup System',
+    'name' => 'Mikrotik Backup System',
     'language' => 'ru-RU',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],

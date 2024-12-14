ee1075bed0e3da5e74ffb19bb85eedab78a0ee0e
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-14 08:30:28 -0400

    Update index.php

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -2,45 +2,41 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Real Mikrotik Backup System';
+$this->title = 'Sistema de Backup Mikrotik';
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Real Mikrotik Backup</h1>
+        <h1 class="display-4">Backup Mikrotik</h1>
 
-        <p class="lead">Сохраняй и контролируй конфигурацию своего сетевого оборудования Mikrotik</p>
+        <p class="lead">Gerencie e salve as configurações dos seus equipamentos Mikrotik de forma fácil e segura.</p>
 
-        <p><a class="btn btn-lg btn-success" href="https://github.com/AntonAndreevichMoroz/RealMikrotikBackup">Github</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://steinhorst.eti.br">Visite nosso site</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Документация</h2>
+                <h2>Documentação</h2>
 
-                <p>Система является максимально простой и интуитивно понятной, но мы все таки пытаемся вести документацию о настройках и использовании продукта. Ведем ее в виде
-                   Wiki страницы на Github. Вы так же можете присоединиться к написанию документации.</p>
+                <p>O sistema foi desenvolvido para ser simples e intuitivo. Ainda assim, disponibilizamos uma documentação completa para ajudá-lo na configuração e uso do produto. Você também pode colaborar com o desenvolvimento da documentação na nossa página Wiki no GitHub.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://github.com/AntonAndreevichMoroz/RealMikrotikBackup/wiki">Wiki страница &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href="https://github.com/steinhorstbr/Webmk-Backup/">Acesse a Wiki &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Техподдержка</h2>
+                <h2>Suporte Técnico</h2>
 
-                <p>Продукт предоставляется по принципу &quot;как есть&quot;. Но вы можете задать вопрос, а так же сообщить об ошибке или запросить дополнительную функцию на Github.
-                   По возможности ошибки будут исправлены, а запросы реализованы и выпущены в новых релизах.</p>
+                <p>Embora o produto seja fornecido "como está", estamos disponíveis para responder suas dúvidas, corrigir eventuais problemas ou implementar novas funcionalidades sugeridas. Utilize nosso Issue Tracker no GitHub para relatar problemas ou enviar sugestões.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://github.com/AntonAndreevichMoroz/RealMikrotikBackup/issues">Issue tracker &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href="https://github.com/steinhorstbr/Webmk-Backup/">Relatar Problema &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Участие в разработке</h2>
+                <h2>Participe do Desenvolvimento</h2>
 
-                <p>Мы будем рады, если Вы захотите поучаствовать в проекте и сделать этот продукт лучше. Любые Ваши способности как разарботчика ПО или веб,
-                   а так же сетевого администратора MIkrotik будут полезны. Вы сможете привнести свои идеи и доработки, поделившись ими с сообществом Mikrotik. Для этого напишите письмо и
-                   мы обсудим Ваше присоединение к команде на Github</p>
+                <p>Quer contribuir para tornar este projeto ainda melhor? Sua ajuda como desenvolvedor ou administrador de redes Mikrotik será muito bem-vinda. Compartilhe suas ideias e melhorias com a comunidade Mikrotik. Entre em contato conosco e discutiremos como você pode se juntar à equipe no GitHub.</p>
 
-                <p><a class="btn btn-outline-secondary" href="mailto:moroz@llcreal.ru?subject=Участие в проекте RealMikrotikBackup">Написать письмо &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href="mailto:rodrigo@steinhorst.eti.br?subject=Participar do Projeto MikrotikBackup">Entre em Contato &raquo;</a></p>
             </div>
         </div>
 

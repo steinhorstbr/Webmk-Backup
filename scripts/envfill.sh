c7e23a204ff7bf04e1f67631e682f726aa831fb9
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-14 07:53:37 -0400

    Update envfill.sh

--- scripts/envfill.sh
+++ scripts/envfill.sh
@@ -3,47 +3,47 @@ NC='\033[0m'
 
 while :
 do
-	read -r -s -p "Введите пароль для пользователя MySQL: " MYSQL_PASSWORD
+	read -r -s -p "Digite a senha do usuário MySQL " MYSQL_PASSWORD
 	if [[ $MYSQL_PASSWORD != "" ]]
 	then
 		sed -i "s/^MYSQL_PASSWORD.*$/MYSQL_PASSWORD=$MYSQL_PASSWORD/g" .env
 		echo
 		break
 	else
-		echo; echo -e "${RED}Пароль не может быть пустым${NC}"
+		echo; echo -e "${RED}A senha não pode ficar vazia${NC}"
 		continue
 	fi
 done
 
 while :
 do
-	read -r -s -p "Введите пароль для ROOT пользователя MariaDB: " MARIADB_ROOT_PASSWORD
+	read -r -s -p "Digite a senha do usuário ROOT do MariaDB: " MARIADB_ROOT_PASSWORD
 	if [[ $MARIADB_ROOT_PASSWORD != "" ]]
 	then
 		sed -i "s/^MARIADB_ROOT_PASSWORD.*$/MARIADB_ROOT_PASSWORD=$MARIADB_ROOT_PASSWORD/g" .env
 		echo
 		break
 	else
-		echo; echo -e "${RED}Пароль не может быть пустым${NC}"
+		echo; echo -e "${RED}A senha não pode ficar vazia${NC}"
 		continue
 	fi
 done
 
 while :
 do
-        read -r -p "Хотите использовать шифрование для паролей в базе данных? [Y\n] " DATA_ENCRYPT
+        read -r -p "Quer usar criptografia para as senhas do seu banco de dados? [Y\n] " DATA_ENCRYPT
         if [[ $DATA_ENCRYPT =~ ^(Y|y)$ ]]
         then
                 while :
                 do
-                        read -r -s -p "Введите пароль для шифрования: " DATA_ENCRYPT_PASSWORD
+                        read -r -s -p "Digite a senha de criptografia: " DATA_ENCRYPT_PASSWORD
                         if [[ $DATA_ENCRYPT_PASSWORD != "" ]]
                         then
                                 sed -i "s/^#\?DATA_ENCRYPT_PASSWORD.*$/DATA_ENCRYPT_PASSWORD=$DATA_ENCRYPT_PASSWORD/g" .env
                                 echo
                                 break 2
                         else
-                                echo; echo -e "${RED}Пароль не может быть пустым${NC}"
+                                echo; echo -e "${RED}A senha não pode ficar vazia${NC}"
                                 continue
                         fi
                 done
@@ -52,14 +52,14 @@ do
                 sed -i "s/^DATA_ENCRYPT_PASSWORD.*$/#DATA_ENCRYPT_PASSWORD=/g" .env
                 break
         else
-                echo -e "${RED}Сделайте правильный выбор${NC}"
+                echo -e "${RED}Faça a escolha certa${NC}"
                 continue
         fi
 done
 
 while :
 do
-	read -r -p "Хотите использовать бекапы на основании экспорта? [Y\n] " MK_BACKUP_EXPORT
+	read -r -p "Deseja usar backups baseados em exportações? [Y\n] " MK_BACKUP_EXPORT
 	if [[ $MK_BACKUP_EXPORT =~ ^(Y|y)$ ]]
 	then
 		sed -i "s/^MK_BACKUP_EXPORT.*$/MK_BACKUP_EXPORT=true/g" .env
@@ -69,7 +69,7 @@ do
 		sed -i "s/^MK_BACKUP_EXPORT.*$/MK_BACKUP_EXPORT=false/g" .env
 		break
 	else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}Escolha${NC}"
 		continue
 	fi
 done
@@ -79,7 +79,7 @@ if [[ $MK_BACKUP_EXPORT =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Хотите использовать использовать парамерт HIDE_SENSITIVE при экспорте? [Y\n] " MK_BACKUP_HIDE_SENSITIVE
+		read -r -p "Deseja usar o parâmetro HIDE_SENSITIVE ao exportar? [Y\n] " MK_BACKUP_HIDE_SENSITIVE
 		if [[ $MK_BACKUP_HIDE_SENSITIVE =~ ^(Y|y)$ ]]
 		then
 			sed -i "s/^MK_BACKUP_HIDE_SENSITIVE.*$/MK_BACKUP_HIDE_SENSITIVE=true/g" .env
@@ -89,7 +89,7 @@ then
 			sed -i "s/^MK_BACKUP_HIDE_SENSITIVE.*$/MK_BACKUP_HIDE_SENSITIVE=false/g" .env
 			break
 		else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}Escolha${NC}"
 			continue
 		fi
 	done
@@ -97,7 +97,7 @@ fi
 
 while :
 do
-	read -r -p "Отправлять экспортные бекапы в Git? [Y\n] " GIT_USING
+	read -r -p "Enviar backups de exportação para o Git? [Y\n] " GIT_USING
 	if [[ $GIT_USING =~ ^(Y|y)$ ]]
 	then
 		sed -i "s/^GIT_USING.*$/GIT_USING=true/g" .env
@@ -107,7 +107,7 @@ do
 		sed -i "s/^GIT_USING.*$/GIT_USING=false/g" .env
 		break
 	else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}Escolha${NC}"
 		continue
 	fi
 done
@@ -116,13 +116,13 @@ if [[ $GIT_USING =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Укажите URL репозитория Git: " GIT_URL
+		read -r -p "Especifique o URL do repositório Git: " GIT_URL
 		if [[ $GIT_URL != "" ]]
 		then
 			sed -i "s~^GIT_URL.*$~GIT_URL=$GIT_URL~g" .env
 			break
 		else
-		echo -e "${RED}Значение не должно быть пустым${NC}"
+		echo -e "${RED}O valor não deve estar vazio${NC}"
 			continue
 		fi
 	done
@@ -132,13 +132,13 @@ if [[ $GIT_USING =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Укажите имя пользователя для авторизации в репозитории Git: " GIT_USER
+		read -r -p "Forneça um nome de usuário para fazer login no repositório Git: " GIT_USER
 		if [[ $GIT_USER != "" ]]
 		then
 			sed -i "s/^GIT_USER.*$/GIT_USER=$GIT_USER/g" .env
 			break
 		else
-		echo -e "${RED}Значение не должно быть пустым${NC}"
+		echo -e "${RED}O valor não deve estar vazio${NC}"
 			continue
 		fi
 	done
@@ -148,14 +148,14 @@ if [[ $GIT_USING =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -s -p "Укажите пароль для авторизации в репозитории Git: " GIT_PASSWORD
+		read -r -s -p "Especifique a senha para autorização no repositório Git: " GIT_PASSWORD
 		if [[ $GIT_PASSWORD != "" ]]
 		then
 			sed -i "s/^GIT_PASSWORD.*$/GIT_PASSWORD=$GIT_PASSWORD/g" .env
 			echo
 			break
 		else
-		echo; echo -e "${RED}Значение не должно быть пустым${NC}"
+		echo; echo -e "${RED}O valor não deve estar vazio${NC}"
 			continue
 		fi
 	done
@@ -165,13 +165,13 @@ if [[ $GIT_USING =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Укажите значение для поля user.name: " GIT_CONFIG_USERNAME
+		read -r -p "Especifique o valor para o campo user.name: " GIT_CONFIG_USERNAME
 		if [[ $GIT_CONFIG_USERNAME != "" ]]
 		then
 			sed -i "s/^GIT_CONFIG_USERNAME.*$/GIT_CONFIG_USERNAME=$GIT_CONFIG_USERNAME/g" .env
 			break
 		else
-		echo -e "${RED}Значение не должно быть пустым${NC}"
+		echo -e "${RED}O valor não deve estar vazio${NC}"
 			continue
 		fi
 	done
@@ -181,13 +181,13 @@ if [[ $GIT_USING =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Укажите значение для поля user.mail: " GIT_CONFIG_USERMAIL
+		read -r -p "Especifique o valor para o campo user.mail: " GIT_CONFIG_USERMAIL
 		if [[ $GIT_CONFIG_USERMAIL != "" ]]
 		then
 			sed -i "s/^GIT_CONFIG_USERMAIL.*$/GIT_CONFIG_USERMAIL=$GIT_CONFIG_USERMAIL/g" .env
 			break
 		else
-		echo -e "${RED}Значение не должно быть пустым${NC}"
+		echo -e "${RED}O valor não deve estar vazio${NC}"
 			continue
 		fi
 	done
@@ -195,7 +195,7 @@ fi
 
 while :
 do
-	read -r -p "Хотите использовать бинарные бекапы? [Y\n] " MK_BACKUP_BINARY
+	read -r -p "Você deseja usar backups binários? [Y\n] " MK_BACKUP_BINARY
 	if [[ $MK_BACKUP_BINARY =~ ^(Y|y)$ ]]
 	then
 		sed -i "s/^MK_BACKUP_BINARY.*$/MK_BACKUP_BINARY=true/g" .env
@@ -205,7 +205,7 @@ do
 		sed -i "s/^MK_BACKUP_BINARY.*$/MK_BACKUP_BINARY=false/g" .env
 		break
 	else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}O valor não deve estar vazio${NC}"
 		continue
 	fi
 done
@@ -214,19 +214,19 @@ if [[ $MK_BACKUP_BINARY =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Хотите использовать шифрование для бинарных бекапов? [Y\n] " MK_BACKUP_ENCRYPT
+		read -r -p "Você deseja usar criptografia para backups binários? [Y\n] " MK_BACKUP_ENCRYPT
 		if [[ $MK_BACKUP_ENCRYPT =~ ^(Y|y)$ ]]
 		then
 			while :
 			do
-				read -r -s -p "Введите пароль для шифрования: " MK_BACKUP_ENCRYPT_PASSWORD
+				read -r -s -p "Digite sua senha de criptografia: " MK_BACKUP_ENCRYPT_PASSWORD
 				if [[ $MK_BACKUP_ENCRYPT_PASSWORD != "" ]]
 				then
 					sed -i "s/^#\?MK_BACKUP_ENCRYPT_PASSWORD.*$/MK_BACKUP_ENCRYPT_PASSWORD=$MK_BACKUP_ENCRYPT_PASSWORD/g" .env
 					echo
 					break 2
 				else
-					echo; echo -e "${RED}Пароль не может быть пустым${NC}"
+					echo; echo -e "${RED}A senha não pode ficar vazia${NC}"
 					continue
 				fi
 			done
@@ -235,7 +235,7 @@ then
 			sed -i "s/^MK_BACKUP_ENCRYPT_PASSWORD.*$/#MK_BACKUP_ENCRYPT_PASSWORD=/g" .env
 			break
 		else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}Faça a escolha certa${NC}"
 			continue
 		fi
 	done
@@ -243,7 +243,7 @@ fi
 
 while :
 do
-	read -r -p "Проводить автоматическое удаление старых файлов бинарных бекапов? [Y\n] " PURGE_OLD_BACKUP
+	read -r -p "Excluir automaticamente arquivos de backup binários antigos? [Y\n] " PURGE_OLD_BACKUP
 	if [[ $PURGE_OLD_BACKUP =~ ^(Y|y)$ ]]
 	then
 		sed -i "s/^PURGE_OLD_BACKUP.*$/PURGE_OLD_BACKUP=true/g" .env
@@ -253,7 +253,7 @@ do
 		sed -i "s/^PURGE_OLD_BACKUP.*$/PURGE_OLD_BACKUP=false/g" .env
 		break
 	else
-		echo -e "${RED}Сделайте правильный выбор${NC}"
+		echo -e "${RED}Escolha${NC}"
 		continue
 	fi
 done
@@ -262,13 +262,13 @@ if [[ $PURGE_OLD_BACKUP =~ ^(Y|y)$ ]]
 then
 	while :
 	do
-		read -r -p "Сколько последних копий оставлять при автоматическом удалении? [число] " PURGE_N_PIECE
+		read -r -p "Quantas últimas cópias devo manter durante a exclusão automática? [число] " PURGE_N_PIECE
 		if [[ $PURGE_N_PIECE -ge 1 ]]
 		then
 			sed -i "s/^PURGE_N_PIECE.*$/PURGE_N_PIECE=$PURGE_N_PIECE/g" .env
 			break
 		else
-		echo -e "${RED}Введите чило больше 1${NC}"
+		echo -e "${RED}Digite um número maior que 1${NC}"
 			continue
 		fi
 	done
